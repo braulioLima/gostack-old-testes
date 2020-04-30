bac2a9f4cf07d7f18cbda747b0c2bfbbee90c659
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2020-04-30 13:14:04 -0300

    chore: initial commit

---
+++ server/src/app/models/User.js
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.VIRTUAL,
+        password_hash: DataTypes.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  // static associate(models) {}
+}
+
+export default User;
